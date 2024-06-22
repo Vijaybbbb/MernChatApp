675cf08d453dff42dfda658d5b5b75902b4f0602
Vijay Ram kp <135154698+Vijaybbbb@users.noreply.github.com>
2024-06-22 23:42:42 +0530

    d

--- Backend/index.js
+++ Backend/index.js
@@ -1,103 +1,206 @@
-const express = require('express') 
-const cors = require('cors') 
-const app  = express()
-const dotenv = require('dotenv')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser'); 
+// const express = require('express') 
+// const cors = require('cors') 
+// const app  = express()
+// const dotenv = require('dotenv')
+// const cookieParser = require('cookie-parser')
+// const bodyParser = require('body-parser'); 
 
-const { connect } = require('./Utils/databaseConnection')
-dotenv.config()
-const PORT =  process.env.PORT || 3000
-connect()
-const userRouter = require('./Router/user')
-const chatRouter = require('./Router/chat')
-const messageRouter = require('./Router/message')
+// const { connect } = require('./Utils/databaseConnection')
+// dotenv.config()
+// const PORT =  process.env.PORT || 3000
+// connect()
+// const userRouter = require('./Router/user')
+// const chatRouter = require('./Router/chat')
+// const messageRouter = require('./Router/message')
 
 
-//middlewares 
-app.use(express.json()) 
-app.use(cookieParser())
+// //middlewares 
+// app.use(express.json()) 
+// app.use(cookieParser())
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+
+
+// app.use(cors({
+//        origin: 'https://magnificent-cactus-8eecd6.netlify.app',
+//        credentials: true 
+//        }))
+
+
+
+// app.use('/user',userRouter)
+// app.use('/chat',chatRouter)
+// app.use('/message',messageRouter)
+
+
+
+// app.use((err,req,res,next)=>{
+//        const errorStatus  = err.status || 500
+//        const errorMessage  = err.message || 'Something Went Wrong'
+
+//        return res.status(errorStatus).json({
+//               success:false,
+//               status:errorStatus,
+//               message:errorMessage,
+//               stack:err.stack
+//        })
+// })
+
+// app.post('/clearCookie', (req, res) => {
+//        // Set the cookie's expiration date to a past time
+//        try {
+//               res.cookie('access_tocken', '', { expires: new Date(0) });
+//               // Send a response
+//               res.status(200).json('Cookie cleared'); 
+//        } catch (error) {
+//               console.log(error);
+//        }
+//    });
+
+// const server = app.listen(PORT,()=>{`Server Started On Port 3000`})
+
+// // Socket.io setup with CORS configuration
+// const io = require('socket.io')(server, {
+//        pingTimeout: 60000,
+//        cors: {
+//            origin: 'https://magnificent-cactus-8eecd6.netlify.app',
+//            credentials: true,
+//        },
+//    });
+
+// io.on('connection',(socket)=>{
+//        //console.log("conected to Socket.io");
+//        socket.on('setup',(userData)=>{
+//               socket.join(userData)
+//              // console.log(userData);
+//               socket.emit('connected')
+
+//               socket.on('join chat',(room)=>{
+//                     // console.log( room);
+//                      socket.join(room)
+//                      //console.log('user joined to ',+ room);    
+//               })
+
+//               socket.on('typing',(room)=>socket.in(room).emit('typing'))
+//               socket.on('stop typing',(room)=>socket.in(room).emit('stop typing'))
+
+//               socket.on('new message',(newMessageRecived)=>{
+
+//                      var chat  = newMessageRecived.chat;   
+//                      if(!chat ) return console.log('chat not defined');
+//                      //console.log( chat);
+
+//                      chat.users.forEach(userData => {
+//                             if(userData._id == newMessageRecived.sender._id) return;
+
+//                             socket.in(userData._id).emit('message recieved' , newMessageRecived);
+
+//                      });
+
+//               })
+//        })
+// })
+
+
+
+
+
+
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const { connect } = require('./Utils/databaseConnection');
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+connect();
+const userRouter = require('./Router/user');
+const chatRouter = require('./Router/chat');
+const messageRouter = require('./Router/message');
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-
+// CORS configuration
+const allowedOrigins = ['https://magnificent-cactus-8eecd6.netlify.app'];
 app.use(cors({
-       origin: 'https://magnificent-cactus-8eecd6.netlify.app',
-       credentials: true 
-       }))
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true); // Allow requests with no origin (like mobile apps or curl requests)
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
 
+app.use('/user', userRouter);
+app.use('/chat', chatRouter);
+app.use('/message', messageRouter);
 
-
-app.use('/user',userRouter)
-app.use('/chat',chatRouter)
-app.use('/message',messageRouter)
-
-
-
-app.use((err,req,res,next)=>{
-       const errorStatus  = err.status || 500
-       const errorMessage  = err.message || 'Something Went Wrong'
-
-       return res.status(errorStatus).json({
-              success:false,
-              status:errorStatus,
-              message:errorMessage,
-              stack:err.stack
-       })
-})
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || 'Something Went Wrong';
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack
+  });
+});
 
 app.post('/clearCookie', (req, res) => {
-       // Set the cookie's expiration date to a past time
-       try {
-              res.cookie('access_tocken', '', { expires: new Date(0) });
-              // Send a response
-              res.status(200).json('Cookie cleared'); 
-       } catch (error) {
-              console.log(error);
-       }
-   });
+  try {
+    res.cookie('access_tocken', '', { expires: new Date(0) });
+    res.status(200).json('Cookie cleared');
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-const server = app.listen(PORT,()=>{`Server Started On Port 3000`})
+const server = app.listen(PORT, () => {
+  console.log(`Server Started On Port ${PORT}`);
+});
 
 // Socket.io setup with CORS configuration
 const io = require('socket.io')(server, {
-       pingTimeout: 60000,
-       cors: {
-           origin: 'https://magnificent-cactus-8eecd6.netlify.app',
-           credentials: true,
-       },
-   });
+  pingTimeout: 60000,
+  cors: {
+    origin: 'https://magnificent-cactus-8eecd6.netlify.app',
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
+});
 
-io.on('connection',(socket)=>{
-       //console.log("conected to Socket.io");
-       socket.on('setup',(userData)=>{
-              socket.join(userData)
-             // console.log(userData);
-              socket.emit('connected')
+io.on('connection', (socket) => {
+  socket.on('setup', (userData) => {
+    socket.join(userData);
+    socket.emit('connected');
 
-              socket.on('join chat',(room)=>{
-                    // console.log( room);
-                     socket.join(room)
-                     //console.log('user joined to ',+ room);    
-              })
+    socket.on('join chat', (room) => {
+      socket.join(room);
+    });
 
-              socket.on('typing',(room)=>socket.in(room).emit('typing'))
-              socket.on('stop typing',(room)=>socket.in(room).emit('stop typing'))
+    socket.on('typing', (room) => socket.in(room).emit('typing'));
+    socket.on('stop typing', (room) => socket.in(room).emit('stop typing'));
 
-              socket.on('new message',(newMessageRecived)=>{
+    socket.on('new message', (newMessageRecived) => {
+      const chat = newMessageRecived.chat;
+      if (!chat) return console.log('chat not defined');
 
-                     var chat  = newMessageRecived.chat;   
-                     if(!chat ) return console.log('chat not defined');
-                     //console.log( chat);
-
-                     chat.users.forEach(userData => {
-                            if(userData._id == newMessageRecived.sender._id) return;
-
-                            socket.in(userData._id).emit('message recieved' , newMessageRecived);
-
-                     });
-
-              })
-       })
-})
+      chat.users.forEach(userData => {
+        if (userData._id == newMessageRecived.sender._id) return;
+        socket.in(userData._id).emit('message received', newMessageRecived);
+      });
+    });
+  });
+});
